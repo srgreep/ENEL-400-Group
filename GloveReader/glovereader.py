ee9ba90ae6d1a4b4fd405a32509757b6c4c3bff7
Max <maxmcevoy@gmail.com>
2024-02-13 11:37:14 -0700

    new glove reader code

--- GloveReader/glovereader.py
+++ GloveReader/glovereader.py
@@ -1,38 +1,45 @@
 import serial
 import matplotlib.pyplot as plt
-ser = serial.Serial('COM14');
-
-fig, ax = plt.subplots()
-fingers = ['pinky', 'ring', 'middle', 'pointer', 'thumb']
-flexPercent =[0,0,0,0,0]
-bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:purple']
-
-bars = ax.bar(fingers, flexPercent, color=bar_colors)
-ax.set_title("Finger Flex")
-ax.set_ylim(0, 1023)  # Assuming sensor values are in the range 0-1023
-# Function to update the plot
-def update_plot():
-    for bar, value in zip(bars, flexPercent):
-        bar.set_height(value)
-    fig.canvas.draw()
-    plt.pause(0.001)
 
 
+port = 'COM9'
 while True:
-    input = ser.read(2)
-    finger = input[0]>>5
-    value = (input[0] & 0b011100000) + input[1]
-    match finger:
-        case 0b000:
-            flexPercent[0] = value
-        case 0b001:
-            flexPercent[1] = value
-        case 0b010:
-            flexPercent[2] = value
-        case 0b011:
-            flexPercent[3] = value
-        case 0b100:
-            flexPercent[4] = value
-        case _:
-            print("DANGER")
-    update_plot()
+    try:
+        ser = serial.Serial(port, 9600)
+        break
+    except serial.SerialException:
+        port = input("Invalid port try again: ")
+
+pinky, ring, middle, pointer, thumb = 78, 79, 80, 81, 82
+# fig, ax = plt.subplots()
+# fingers = ['pinky', 'ring', 'middle', 'pointer', 'thumb']
+flexPercent =[0,0,0,0,0]
+# bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:purple']
+
+# bars = ax.bar(fingers, flexPercent, color=bar_colors)
+# ax.set_title("Finger Flex")
+# ax.set_ylim(0, 4050)  # Assuming sensor values are in the range 0-4050
+# # Function to update the plot
+# def update_plot():
+#     for bar, value in zip(bars, flexPercent):
+#         bar.set_height(value)
+#     fig.canvas.draw()
+#     plt.pause(0.001)
+
+
+ser.write(pinky)
+flexPercent[0] = ser.read(2)
+ser.write(ring)
+flexPercent[1] = ser.read(2)
+ser.write(middle)
+flexPercent[2] = ser.read(2)
+ser.write(pointer)
+flexPercent[3] = ser.read(2)
+ser.write(thumb)
+flexPercent[3] = ser.read(2)
+
+print(flexPercent)
+ser.close()
+
+
+# update_plot()
